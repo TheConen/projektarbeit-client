ef20074806ca657ebb89c7dfb44b13393b476863
TheConen <theconen@gmail.com>
2016-06-18 22:33:12 +0200

    removed overwriting of mail address with testing address

--- src/main/java/projektarbeit/client/callforhelp/CallForHelpMessageHandler.java
+++ src/main/java/projektarbeit/client/callforhelp/CallForHelpMessageHandler.java
@@ -67,7 +67,6 @@ public class CallForHelpMessageHandler extends MessageHandler {
                         break;
                 }
 
-                recipient = "johannesconen@gmail.com";
                 mail = new MailSender(recipient);
                 mail.send();
                 break;
