4333d758acc3b34a636a3f81545ed9d25cb2ad62
TheConen <theconen@gmail.com>
2016-06-20 22:38:23 +0200

    finished implementation of Big Brother

--- src/main/java/projektarbeit/client/bigbrother/BigBrotherMessageHandler.java
+++ src/main/java/projektarbeit/client/bigbrother/BigBrotherMessageHandler.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public class BigBrotherMessageHandler extends MessageHandler {
 
+    static private Process p;
+    final private String address = "http://192.168.1.10/videostream.cgi?user=admin&pwd=Abcd123.&rate=4096";
+
     public BigBrotherMessageHandler(List<String> message) {
         super(message);
     }
@@ -19,13 +22,23 @@ public class BigBrotherMessageHandler extends MessageHandler {
 
     @Override
     protected String onLaunch() {
-        return "Big Brother Launch Request";
+        String answer = openBigBrother();
+        return answer;
     }
 
     @Override
     protected String onIntent(Intent intent) {
         String intentName = intent.getName();
-        String answer = "Big Brother Intent Request";
+        String answer = null;
+        switch (intentName){
+            case "Open":
+                answer = openBigBrother();
+                break;
+            case "Close":
+                p.destroy();
+                answer = "I closed Big Brother on your computer";
+                break;
+        }
 
         return answer;
     }
@@ -33,6 +46,22 @@ public class BigBrotherMessageHandler extends MessageHandler {
     @Override
     protected void onSessionEnded() {
 
+    }
+
+    private String openBigBrother() {
+        String answer = null;
+        if (p == null || !p.isAlive()) {
+            try {
+                p = new ProcessBuilder("C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe", address, "-f").start();
+                answer = "I opened Big Brother on your computer";
+            } catch (IOException e) {
+                answer = "An error occured opening Big Brother";
+                e.printStackTrace();
+            }
+        } else {
+            answer = "Big Brother is already running on your computer";
+        }
+        return answer;
     }
 }
 
